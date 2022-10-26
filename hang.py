8495cf019e88caa3667fcc650f3b969ab7a539fc
EdgeEden <81897525+EdgeEden@users.noreply.github.com>
2022-10-26 21:54:51 +0800

    Update hang.py

--- hang.py
+++ hang.py
@@ -1,11 +1,11 @@
 import pynput
 import time
 control = pynput.mouse.Controller()
-n = 1
+n = -1
 while True:
-    n = n+1
+    n = n * (-1)
     pos1 = control.position
-    control.move(10*(-1)**n, 10*(-1)**n)
+    control.move(10*n, 10*n)
     time.sleep(2)
     pos2 = control.position
     if abs(pos2[0]-pos1[0]) > 100 | abs(pos2[1]-pos1[1]) > 100:

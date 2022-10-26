82b38ba999447762c04f48b43f737180e41f9038
EdgeEden <81897525+EdgeEden@users.noreply.github.com>
2022-10-26 22:01:14 +0800

    Update lan.py

--- lan.py
+++ lan.py
@@ -5,7 +5,7 @@ while True:
     res = os.system('ping 8.8.8.8')
     if res:
         os.system('Rasdial 宽带连接 /DISCONNECT')
-        os.system('Rasdial 宽带连接 hzqta57196572 459630')
+        os.system('Rasdial 宽带连接 账号 密码')#  删掉“账号”两字后在原处输入宽带账号 密码同理
         time.sleep(5)
         pass
     else:
